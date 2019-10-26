3b4078b75f685a8744f3916359471a7ddf2db72a
James <jacknme0@gmail.com>
2019-10-26 22:40:38 +0800

    revamped directories

---
+++ modules/mongo.R
@@ -0,0 +1,34 @@
+library(tidyr)
+
+# load login variables
+source("modules/login.R")
+#login.R should have two variables
+# user<-"username"
+# pass<-"password"
+
+
+#set up collection to query
+availabilityCollection<-mongo(collection="carpark",db = "test_db",
+               url=sprintf('mongodb+srv://%s:%s@james-cluster-bfs0h.gcp.mongodb.net',user,pass))
+
+getCarpark<-function(mongo_collection=availabilityCollection, carpark_id, limit=100){
+  fields<-sprintf('{"%s" : 1, "time" :1, "_id" :0}', carpark_id) #query the carpark_id
+  mongo_collection$find('{}' , limit = limit, fields = fields, sort='{"time":-1}')
+}
+
+getAllCarparks<-function(mongo_collection=availabilityCollection, limit=100, fake = FALSE){
+  if (fake == TRUE){
+    return(readRDS("backup"))
+  }
+  else {
+    #get latest carpark from mongo
+    df<-carpark$find('{}' , limit = limit, sort='{"time":-1}')
+    #reshape into tidy
+    df %>% gather(key=carpark_name,value = avail_lots, -time)
+  }
+
+}
+
+#in case mongo does not work
+# backup<-getAllCarparks(limit=100)
+# backup %>% saveRDS("backup")
