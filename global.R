1e0c867284162af44be968428c86924793469e75
James <jacknme0@gmail.com>
2019-11-08 02:04:26 +0800

    made default using fake data

--- global.R
+++ global.R
@@ -24,7 +24,7 @@ source("modules/create_arima.R")
 ### Static variables ### Variables available to all sessions
 
 #James stuff
-fake<-FALSE #use fake=TRUE to avoid calling mongodb and use saved carpark data instead of realtime
+fake<-TRUE #use fake=TRUE to avoid calling mongodb and use saved carpark data instead of realtime
 Sys.setenv(TZ="Asia/Singapore") #to avoid mongo messing up the timezone
 TIME_INTERVAL<-5 #5 minutes, used for prediction intervals
 #End James stuff
