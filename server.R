8b8d927967f65ddb00d56f94a9e71bbcaa223473
James <jacknme0@gmail.com>
2019-11-07 16:20:41 +0800

    css updated. added weather icons. remove predicted tab. added weather on top right

--- server.R
+++ server.R
@@ -1,12 +1,15 @@
 source("global_stuff.R", local=FALSE)
 server <- function(input, output, session, ...) {
   
+  ###### START SETUP OF SESSION VARIABLES ########
+  
+  
   # Load dataset which is run ech time user visits, so that it will be refreshed
   ### load carpark available dataset from mongo only when initialized
-  carparkAvail<-getAllCarparks(limit=288, fake=TRUE)
+  carparkAvail<-getAllCarparks(limit=288, fake=fake)
   uniqueCarparks <- unique(carparkAvail$carpark_name)
-  ### load latest time from mongo
-  latestTime<-carparkAvail$time[[1]]
+  ### load latest time from mongo and set it for all sessions
+  latestTime<<-carparkAvail$time[[1]]
   
   ### get predicted carpark info
   prediction<-get_prediction_historical_2(latestTime=latestTime, carparkAvail=carparkAvail,
@@ -35,7 +38,6 @@ server <- function(input, output, session, ...) {
   #Make reactive filtered dataset based on inputs in sidebar
   carpark_info_reactive <- reactive({
     cat("Debug: updating carpark filter\n")
-    cat(input$chosen_avail)
     hdb_geo_info_reactive() %>%
       filter(car_park_type %in% input$chosen_carparks) %>%
       filter(lot_avail_colour %in% input$chosen_avail) #filter based on colour
@@ -52,6 +54,38 @@ server <- function(input, output, session, ...) {
       map <- leaflet() %>% addTiles(options = tileOptions(useCache = TRUE, crossOrigin = TRUE)) %>% 
         setView(lng = 103.7499, lat =1.350867, zoom  = 12) 
     }) 
+  
+  
+  
+  
+  # More weather stuff
+  weather_data <- data.frame("Area"= data_area[c(1:5),2],"Weather" = c(1:5),"Temperature" = c(1:5))
+  for (i in 1:nrow(data_area)) {
+    url3 <- "https://api.openweathermap.org/data/2.5/weather?"
+    lat <- paste0("lat=",as.character(data_area$label_location.latitude[i]))
+    lon <- paste0("&lon=",as.character(data_area$label_location.longitude[i]))
+    appID<- "&APPID=a1e683f48fd044a44af31c3849470646"
+    units <- "&units=metric"
+    url_comp <-paste0(url3,lat,lon,appID,units)
+    data_loop<-fromJSON(url_comp)
+    weather_data$Temperature[i] <- data_loop$main$temp
+    weather_data$Weather[i] <- data_loop$weather$main
+  }
+  
+  output$weather_menu <- renderMenu({
+    items <- lapply(1:5, function(i){
+      notificationItem(paste0(weather_data$Area[i],": ", weather_data$Weather[i]),
+                       icon = icon('sun-o')) 
+    })
+    dropdownMenu(type = "notification", 
+                 icon = icon("cloud"), 
+                 headerText = "Current Weather Updates",
+                 .list = items
+    )
+  })
+  
+  
+  ###### END SETUP OF SESSION VARIABLES ########
   
   #James to make markers work
   observe({
@@ -94,20 +128,16 @@ server <- function(input, output, session, ...) {
   #Stuff that happens when u click on marker
   observeEvent(input$map_marker_click, {
     #filter out data not in checkbox
-    
     specific_carpark_data <- carpark_info_reactive()[carpark_info_reactive()$car_park_no==input$map_marker_click$id,]
     carpark_table <- specific_carpark_data[,c(1:7)]
     colnames(carpark_table) <- c('Car Park No.', 'Address', 'Car Park Type', 'Type of Parking System','Short-Term Parking', 'Free Parking', 'Night Parking')
     output$table <- renderTable(gather(carpark_table,'',''))
     
-    
-    #add link
+    # create link for google maps directions
     carpark_link <- specific_carpark_data[,c(11:12)]
-    output$link <-renderText(paste0("<a target='_blank', 
-                                    href = https://www.google.com/maps?daddr=", carpark_link$lat,",", carpark_link$lon,"> Go to Google Maps for Directions </a>"))
-  
+    output$link <-renderText(paste0("<a href = https://www.google.com/maps?daddr=", carpark_link$lat,",", carpark_link$lon,"> Go to Google Maps for Directions </a>"))
     
-    ## Weather data stuff
+    #get weather information regarding the selected marker and display it
     url3 <- "https://api.openweathermap.org/data/2.5/weather?"
     lat <- paste0("lat=", carpark_link$lat)
     lon <- paste0("&lon=",carpark_link$lon)
@@ -115,23 +145,24 @@ server <- function(input, output, session, ...) {
     units <- "&units=metric"
     url_comp <-paste0(url3,lat,lon,appID,units)
     weather_data <- fromJSON(url_comp)
-    output$temp <- renderText(paste0("Temperature: ",weather_data$main$temp," Degree Celcius"))
-    output$weather <- renderText(paste0("Weather: ", weather_data$weather$main))
+    weather_table <- data.frame('Weather' = weather_data$weather$main, 'Temperature' = paste(weather_data$main$temp, '\u00B0C'))
+    icon_table <- gather(weather_table,'','')
+    icon_table[,1] <- c(as.character(icon('cloud')), as.character(icon('thermometer-2')))
+    output$weather <- renderTable(icon_table, sanitize.text.function = function(x) x)
     
     
     plot_data <- prediction %>% 
-            filter(carpark_name == paste0(input$map_marker_click$id,"_C"))   %>% 
-            filter(time > latestTime - as.difftime(1, unit="days")) %>%
-            filter(time < latestTime + as.difftime(1, unit="days"))# get one day before and one day after forecast
+      filter(carpark_name == paste0(input$map_marker_click$id,"_C"))   %>% 
+      filter(time > latestTime - as.difftime(1, unit="days")) %>%
+      filter(time < latestTime + as.difftime(1, unit="days"))# get one day before and one day after forecast
+    #plot the graph
     output$plot <- renderPlotly({
-      p <- ggplot(data = plot_data) + geom_line(aes(time, avail_lots, colour=is_pred), size = 0.5) + 
-        xlab("Time") + 
-        ylab("Lots Available") + theme_stata() + scale_x_datetime(breaks = "3 hour", labels = date_format("%H:%M")) 
+      p <- ggplot(data = plot_data) + geom_line(aes(x = time, y = avail_lots, colour = is_pred, group = 1, text = paste0("Available Lots: ", avail_lots))) + xlab("Time") + ylab("Lots Available") + theme_stata() + scale_x_datetime(breaks = "3 hour", labels = date_format("%H:%M", tz = Sys.timezone(location = TRUE))) + theme(text = element_text(family= "Muli"))
       height <- session$clientData$output_p_height
       width <- session$clientData$output_p_width
-      ggplotly(p, height = height, width = width)
+      gg <- ggplotly(p, tooltip = "text", height = height, width = width)
+      gg <- style(gg, line= list(color = '#89a3b2', width = 1.5))
     })
   })
-  
   
 }
--- ui.R
+++ ui.R
@@ -9,22 +9,43 @@ library(plotly)
 
 source("global_stuff.R", local=FALSE)
 
-ui <- dashboardPage(
-  dashboardHeader(title = "Singapore Carpark Availability"),
+ui <- fluidPage(tags$head(
+  #set up css
+  tags$link(rel = "stylesheet", 
+            type = "text/css", 
+            href = "custom.css"),
+  tags$link(rel="stylesheet",
+            href="https://fonts.googleapis.com/css?family=Josefin+Sans&display=swap"),
+  tags$link(rel="stylesheet",
+            href="https://fonts.googleapis.com/css?family=Muli&display=swap"),
+  tags$style(".fa-sun-o {color:#7e97a6}")
+),
+
+dashboardPage(
+  dashboardHeader(
+    title = "parkwhere.sg",
+    dropdownMenuOutput('weather_menu')
+  ),
   dashboardSidebar(
     sidebarMenu(
-      menuItem("Current", tabName = 'current' ),
-      menuItem("Predicted Availability", tabName = 'predict'),
-      id = 'tabselected'
+      menuItem("Current", tabName = 'current' )
     ),
     textInput(inputId = 'address',
               label= "Postal Code/Address",
               value = "-"
     ),
-    fluidRow(column(12, offset = 1,
-                                     timeInput(inputId = 'chosen_time',"Time: ",value = Sys.time(), minute.steps = 30))
-                     ),
-    checkboxGroupInput(inputId = 'chosen_carparks', "Carpark Type:",carpark_types, selected = carpark_types),
+    sliderInput(inputId = 'chosen_time', 
+                                 label = 'time to predict', 
+                                 value = as.POSIXct(latestTime), 
+                                 min = as.POSIXct(latestTime), 
+                                 max = as.POSIXct(latestTime) + 24*60*60, 
+                                 step = 30*60, 
+                                 ticks = FALSE,
+                                 timeFormat = "%m/%d/%Y %I:%M %p"),
+    checkboxGroupInput(inputId = 'chosen_carparks', 
+                       label = "Carpark Type:", 
+                       choices = carpark_types, 
+                       selected = carpark_types),
     checkboxGroupInput(inputId = "chosen_avail",label="Carpark Availability",
                        choiceNames = c("Green > 50","Orange > 20","Red < 20"), choiceValues = c("green", "orange", "red"), 
                        selected = c("green","orange","red")),
@@ -32,18 +53,56 @@ ui <- dashboardPage(
   ),
   
   dashboardBody(
-    tags$head(tags$link(rel="stylesheet", type = "text/css", href = "custom.css")),
-    
     useShinyFeedback(),
     tabItems(
       tabItem(tabName='current', leafletOutput('map'), 
               fluidRow(
                 column(5, plotlyOutput(outputId = "plot")), 
-                column (5, tableOutput('table'), htmlOutput('link')),
-                column(2, textOutput('weather'), textOutput('temp'))
-              )
-              ),
-      tabItem(tabName='predict', h2('output another map'))
+                column (4, tableOutput('table'), htmlOutput('link')),
+                column(3, tableOutput('weather'))
+              ))
     )
   )
 )
+)
+# 
+# 
+# 
+# ui <- dashboardPage(
+#   dashboardHeader(title = "Singapore Carpark Availability"),
+#   dashboardSidebar(
+#     sidebarMenu(
+#       menuItem("Current", tabName = 'current' ),
+#       menuItem("Predicted Availability", tabName = 'predict'),
+#       id = 'tabselected'
+#     ),
+#     textInput(inputId = 'address',
+#               label= "Postal Code/Address",
+#               value = "-"
+#     ),
+#     fluidRow(column(12, offset = 1,
+#                                      timeInput(inputId = 'chosen_time',"Time: ",value = Sys.time(), minute.steps = 30))
+#                      ),
+#     checkboxGroupInput(inputId = 'chosen_carparks', "Carpark Type:",carpark_types, selected = carpark_types),
+#     checkboxGroupInput(inputId = "chosen_avail",label="Carpark Availability",
+#                        choiceNames = c("Green > 50","Orange > 20","Red < 20"), choiceValues = c("green", "orange", "red"), 
+#                        selected = c("green","orange","red")),
+#     actionButton('button', 'Show carparks')
+#   ),
+#   
+#   dashboardBody(
+#     tags$head(tags$link(rel="stylesheet", type = "text/css", href = "custom.css")),
+#     
+#     useShinyFeedback(),
+#     tabItems(
+#       tabItem(tabName='current', leafletOutput('map'), 
+#               fluidRow(
+#                 column(5, plotlyOutput(outputId = "plot")), 
+#                 column (5, tableOutput('table'), htmlOutput('link')),
+#                 column(2, textOutput('weather'), textOutput('temp'))
+#               )
+#               ),
+#       tabItem(tabName='predict', h2('output another map'))
+#     )
+#   )
+# )
